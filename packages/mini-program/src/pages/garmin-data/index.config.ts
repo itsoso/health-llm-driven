0b7ad1b49ac8d55619fe6112139f284accab31e3
itsoso <panbaokun@kuaishou.com>
2026-01-09 07:20:24 +0800

    feat: 小程序添加Garmin数据列表页面，展示最近14天健康数据

---
+++ packages/mini-program/src/pages/garmin-data/index.config.ts
@@ -0,0 +1,6 @@
+export default definePageConfig({
+  navigationBarTitleText: 'Garmin 数据',
+  navigationBarBackgroundColor: '#1a1a2e',
+  navigationBarTextStyle: 'white',
+  backgroundColor: '#1a1a2e',
+});
